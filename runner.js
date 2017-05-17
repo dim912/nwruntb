c70b6f7fa2aa39291eabcb5332a19fa86ef8e6fe
dim912 <dim912@gmail.com>
2017-05-18 01:47:38 +0800

    init commit

---
+++ runner.js
@@ -0,0 +1,15 @@
+var nwrun = require('nwrun')
+nwrun(
+    {
+        standalone: true,
+        "src_folders": [
+            "tests/",
+        ],
+        "output_folder": "./testReports/smartview",
+        "test_workers": { "enabled": true, "workers": "auto" },
+    }
+    , function (done) {
+        if (done === false) {
+            process.exit(1);
+        }
+    });
